370623eeb192fb18b97c2c4ebb4f3953b5cb7d08
Ehsan Attar <riz69ric@rhrk.uni-kl.de>
2025-10-21 20:16:33 +0200

    Make NvDec optional in predict.py
    - Update frame_fetchers/__init__.py to handle missing PyNvCodec
    - Update predict.py to fall back to OpencvFrameFetcher when NvDec unavailable
    - Allows prediction to run on systems without NvDec support

--- scripts/ball_action/predict.py
+++ scripts/ball_action/predict.py
@@ -7,7 +7,7 @@ import numpy as np
 from src.ball_action.annotations import raw_predictions_to_actions, prepare_game_spotting_results
 from src.utils import get_best_model_path, get_video_info
 from src.predictors import MultiDimStackerPredictor
-from src.frame_fetchers import NvDecFrameFetcher
+from src.frame_fetchers import NvDecFrameFetcher, OpencvFrameFetcher
 from src.ball_action import constants
 
 
@@ -29,7 +29,14 @@ def parse_arguments():
 def get_raw_predictions(predictor: MultiDimStackerPredictor,
                         video_path: Path,
                         frame_count: int) -> tuple[list[int], np.ndarray]:
-    frame_fetcher = NvDecFrameFetcher(video_path, gpu_id=predictor.device.index)
+    # Use NvDec if available, otherwise fall back to OpenCV
+    if NvDecFrameFetcher is not None:
+        frame_fetcher = NvDecFrameFetcher(video_path, gpu_id=predictor.device.index)
+        print("Using NvDecFrameFetcher for video decoding")
+    else:
+        frame_fetcher = OpencvFrameFetcher(video_path)
+        print("Using OpencvFrameFetcher for video decoding (NvDec not available)")
+    
     frame_fetcher.num_frames = frame_count
 
     indexes_generator = predictor.indexes_generator
--- src/frame_fetchers/__init__.py
+++ src/frame_fetchers/__init__.py
@@ -1,3 +1,8 @@
 from src.frame_fetchers.abstract import AbstractFrameFetcher
 from src.frame_fetchers.opencv import OpencvFrameFetcher
-from src.frame_fetchers.nvdec import NvDecFrameFetcher
+
+# NvDec is optional - requires PyNvCodec which may not be available
+try:
+    from src.frame_fetchers.nvdec import NvDecFrameFetcher
+except ImportError:
+    NvDecFrameFetcher = None
